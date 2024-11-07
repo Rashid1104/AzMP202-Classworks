0619bfaaa59b225373162c13d4bf515c9cec97be
Shahnazrtm <mi7nx2hs7@code.edu.az>
2024-11-07 19:00:20 +0400

    07.11.2024

---
+++ 07.11.2024/main.js
@@ -0,0 +1,40 @@
+const Header = document.createElement("header");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let DivElement = document.createElement('div');
+// let pElement = document.createElement('p');
+
+// DivElement.style.backgroundColor = "green";
+
+// pElement.textContent = "Azmp202";
+// pElement.style.fontSize = "25px";
+// pElement.style.display = "flex";
+// pElement.style.color= "white";
+// pElement.style.justifyContent = "center";
+
+// DivElement.appendChild(pElement);
+
+// DivElement.style.width = "300px";
+// DivElement.style.height = "300px";
+// DivElement.style.borderRadius = "20px";
+// DivElement.style.border = "solid 2px red";
+
+
+// document.body.appendChild(DivElement);
